d95d7ea9696876951a06c7c487e88b9a7899a28b
Pascal Germain <pascalgermain@me.com>
2017-05-26 19:11:41 +0200

    Add basic game

--- src/components/Bonneteau.vue
+++ src/components/Bonneteau.vue
@@ -1,14 +1,68 @@
 <template>
   <div class="bonneteau">
-
+    <h1>Bonneteau</h1>
+    <ul>
+      <li v-for="(choice, index) in choices">
+        <choice
+          :count="index"
+          :selected="index === choiceSelected"
+          @input="selectChoice($event)"
+        ></choice>
+      </li>
+    </ul>
+    <div
+      v-if="status"
+      class="status"
+      v-text="'You ' + status + '!!!'"
+    ></div>
+    <h2>
+      <a
+        v-if="status"
+        class="restart"
+        href="#"
+        v-text="'Restart'"
+        @click.prevent="start()"
+      ></a>
+    </h2>
   </div>
 </template>
 
 <script>
+import Choice from './choice'
+
 export default {
   name: 'bonneteau',
+  components: {
+    Choice
+  },
   data () {
     return {
+      choicesCount: 3,
+      choices: [],
+      choiceSelected: null,
+      status: null
+    }
+  },
+  computed: {
+    apiUrl () {
+      return `https://www.random.org/integers/?num=1&min=0&max=${this.choicesCount - 1}&col=1&base=10&format=plain&rnd=new`
+    }
+  },
+  created () {
+    this.start()
+  },
+  methods: {
+    start () {
+      this.choices = []
+      this.choiceSelected = null
+      this.status = null
+      for (let i = 0; i < this.choicesCount; i++) this.choices.push(0)
+      this.$http.get(this.apiUrl)
+        .then(response => { this.choiceSelected = parseInt(response.data, 0) })
+        .catch(e => { console.log('error', e) })
+    },
+    selectChoice (count) {
+      this.status = count === this.choiceSelected ? 'win' : 'lose'
     }
   }
 }
@@ -26,10 +80,15 @@ ul {
 
 li {
   display: inline-block;
-  margin: 0 10px;
 }
 
-a {
+.status {
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.restart {
   color: #42b983;
+  text-decoration: none;
 }
 </style>
---
+++ src/components/Choice.vue
@@ -0,0 +1,50 @@
+<template>
+  <a
+    :class="['choice', {selected}]"
+    href="#"
+    @click.prevent="$emit('input', count)"
+  >
+    {{ count + 1 }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'choice',
+  props: {
+    count: {
+      type: Number,
+      required: true
+    },
+    selected: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.choice {
+  display: block;
+  width: 120px;
+  line-height: 120px;
+  margin: 20px;
+  border-radius: 40px;
+  background: #42b983;
+  color: #fff;
+  font-size: 40px;
+  font-weight: bold;
+  text-decoration: none;
+  transform: scale(1);
+}
+
+.choice:hover {
+  transform: scale(1.2);
+  transition: all .25s ease-in-out;
+}
+
+.choice.selected {
+  color: yellow;
+}
+</style>
